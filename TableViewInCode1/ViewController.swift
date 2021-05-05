3206e9c41a8fc9f35579d3b5232e81621ed35b15
Юлия <yuliya.karanevskaya@gmail.com>
2021-05-05 17:46:42 +0300

    Initial Commit

---
+++ TableViewInCode1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewInCode1
+//
+//  Created by Юлия Караневская on 5/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
