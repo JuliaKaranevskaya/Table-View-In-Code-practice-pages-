2f2a6906d960fe21d517221dd1674c6050d99ee0
JuliaK-118 <yuka89.2012@gmail.com>
2021-11-23 11:58:23 +0300

    new commit

--- TableViewInCode2/SecondViewController.swift
+++ TableViewInCode2/SecondViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  TableViewInCode2
 //
 //  Created by Юлия Караневская on 5/5/21.
@@ -7,18 +7,19 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     
     let tableView: UITableView = {
-        let table = UITableView()
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         return table
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(tableView)
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        title = "Grouped"
         tableView.delegate = self
         tableView.dataSource = self
     }
@@ -29,28 +30,40 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        3
+        return 3
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = "Hello world \(indexPath.row)"
         
-        if indexPath.row % 2 == 0 {
+        if indexPath.row == 0 {
             cell.imageView?.image = UIImage(systemName: "bandage")
             cell.imageView?.tintColor = .systemPink
-        } else {
-            cell.imageView?.image = UIImage(systemName: "heart")
-            cell.imageView?.tintColor = .systemRed
+            cell.accessoryType = .checkmark
         }
         
+        if indexPath.row == 1 {
+            cell.imageView?.image = UIImage(systemName: "heart")
+            cell.imageView?.tintColor = .systemRed
+            cell.accessoryType = .detailButton
+        }
+        
+        if indexPath.row == 2 {
+            cell.imageView?.image = UIImage(systemName: "house")
+            cell.imageView?.tintColor = .systemBlue
+            cell.accessoryType = .disclosureIndicator
+        }
+        
+
         return cell
     }
 
 
 }
+
 
